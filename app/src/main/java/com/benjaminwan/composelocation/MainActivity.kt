16a794956ec725c9d634208995a83c4202b94a8a
benjaminwan <benjaminwan@163.com>
2021-08-01 13:26:25 +0800

    add getPermissions

--- app/src/main/java/com/benjaminwan/composelocation/MainActivity.kt
+++ app/src/main/java/com/benjaminwan/composelocation/MainActivity.kt
@@ -1,18 +1,38 @@
 package com.benjaminwan.composelocation
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import com.afollestad.assent.Permission
+import com.afollestad.assent.askForPermissions
+import com.afollestad.assent.isAllGranted
 import com.benjaminwan.composelocation.ui.theme.ComposeLocationTheme
+import com.benjaminwan.composelocation.utils.showToast
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+    private fun getPermissions() {
+        val permissions = arrayOf(
+            Permission.ACCESS_FINE_LOCATION,
+            Permission.ACCESS_COARSE_LOCATION
+        )
+        if (!isAllGranted(*permissions)) {
+            askForPermissions(*permissions) { result ->
+                val permissionGranted: Boolean = result.isAllGranted(*permissions)
+                if (!permissionGranted) {
+                    showToast("权限获取错误！")
+                }
+            }
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        getPermissions()
         setContent {
             ComposeLocationTheme {
                 // A surface container using the 'background' color from the theme
