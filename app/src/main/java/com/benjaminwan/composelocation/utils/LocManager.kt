a62f22d001d5a772fc736f50c1fb94b02bc9345d
benjaminwan <benjaminwan@163.com>
2021-08-03 17:56:26 +0800

    add info logger

--- app/src/main/java/com/benjaminwan/composelocation/utils/LocManager.kt
+++ app/src/main/java/com/benjaminwan/composelocation/utils/LocManager.kt
@@ -8,6 +8,7 @@ import android.os.Bundle
 import androidx.annotation.RequiresApi
 import com.orhanobut.logger.Logger
 
+
 class LocManager(context: Context) {
     companion object {
         const val MIN_TIME_MS: Long = 1000L //更新间隔(毫秒):1000
@@ -65,7 +66,32 @@ class LocManager(context: Context) {
         }
 
         override fun onSatelliteStatusChanged(status: GnssStatus) {
-            Logger.i("onSatelliteStatusChanged ${status}")
+            Logger.i("onSatelliteStatusChanged")
+            (0..status.satelliteCount).forEach {
+                val svid = status.getSvid(it)
+                val constellationType = status.getConstellationType(it)
+                val type = when (constellationType) {
+                    GnssStatus.CONSTELLATION_UNKNOWN -> "UNKNOWN"
+                    GnssStatus.CONSTELLATION_GPS -> "GPS"
+                    GnssStatus.CONSTELLATION_SBAS -> "SBAS"
+                    GnssStatus.CONSTELLATION_GLONASS -> "GLONASS"
+                    GnssStatus.CONSTELLATION_QZSS -> "QZSS"
+                    GnssStatus.CONSTELLATION_BEIDOU -> "BEIDOU"
+                    GnssStatus.CONSTELLATION_GALILEO -> "GALILEO"
+                    GnssStatus.CONSTELLATION_IRNSS -> "IRNSS"
+                    else -> Integer.toString(constellationType)
+                }
+                val cn0DbHz = status.getCn0DbHz(it)
+                val getElevationDegrees = status.getElevationDegrees(it)
+                val azimuthDegrees = status.getAzimuthDegrees(it)
+                val hasEphemerisData = status.hasEphemerisData(it)
+                val hasAlmanacData = status.hasAlmanacData(it)
+                val usedInFix = status.usedInFix(it)
+                val hasCarrierFrequencyHz = status.hasCarrierFrequencyHz(it)
+                val carrierFrequencyHz = status.getCarrierFrequencyHz(it)
+                val hasBasebandCn0DbHz = status.hasBasebandCn0DbHz(it)
+                val basebandCn0DbHz = status.getBasebandCn0DbHz(it)
+            }
         }
     }
 
@@ -75,16 +101,36 @@ class LocManager(context: Context) {
     private fun legacyStatusListener() = object : GpsStatus.Listener {
         override fun onGpsStatusChanged(event: Int) {
             Logger.i("onGpsStatusChanged $event")
-            mLegacyStatus = locationManager.getGpsStatus(mLegacyStatus)
             when (event) {
                 GpsStatus.GPS_EVENT_STARTED -> {
+                    Logger.i("GPS_EVENT_STARTED")
                 }
                 GpsStatus.GPS_EVENT_STOPPED -> {
+                    Logger.i("GPS_EVENT_STOPPED")
                 }
                 GpsStatus.GPS_EVENT_FIRST_FIX -> {
+                    Logger.i("GPS_EVENT_FIRST_FIX")
                 }
                 GpsStatus.GPS_EVENT_SATELLITE_STATUS -> {
-
+                    Logger.i("GPS_EVENT_SATELLITE_STATUS")
+                    mLegacyStatus = locationManager.getGpsStatus(mLegacyStatus)
+                    val gpsStatus = mLegacyStatus
+                    if (gpsStatus != null) {
+                        // 获取卫星颗数的默认最大值
+                        val maxSatellites = gpsStatus.maxSatellites
+                        Logger.i("maxSatellites=$maxSatellites")
+                        // 创建一个迭代器保存所有卫星
+                        gpsStatus?.satellites?.forEach {
+                            Logger.i("$it")
+                        }
+                        val iters: Iterator<GpsSatellite> = gpsStatus.satellites.iterator()
+                        var count = 0
+                        while (iters.hasNext() && count <= maxSatellites) {
+                            val s = iters.next()
+                            Logger.i("$s")
+                            count++
+                        }
+                    }
                 }
             }
         }
